ea714b94dd2ceda4c4beaeaaa33e8a4a449ce5bf
P4vell <kownacki_pawel@o2.pl>
2023-08-23 11:05:43 +0200

    feat: display current category and country on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
-import { Categories } from "../components/categories/Categories";
+import { Categories } from "../components/homepage/Categories";
+import { SearchBar } from "../components/homepage/SearchBar";
+import { useFilters } from "../hooks/useFilters";
 
 const Home = () => {
+  const {
+    filters: { category, country },
+  } = useFilters();
+
   return (
     <>
+      <SearchBar />
       <Categories />
+      <h2 className="text-3xl font-semibold text-slate-900 dark:text-slate-200">
+        {`Top ${category.label} News in ${country.name}`}
+      </h2>
     </>
   );
 };
