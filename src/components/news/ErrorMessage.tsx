226cf2b30c1840d77cc9f7c8145d276d5b3924db
P4vell <kownacki_pawel@o2.pl>
2023-08-31 12:03:14 +0200

    feat: add styles for error message

--- src/components/news/ErrorMessage.tsx
+++ src/components/news/ErrorMessage.tsx
@@ -8,7 +8,7 @@ type ErrorMessageProps = {
 export const ErrorMessage = ({ message }: ErrorMessageProps) => {
   return (
     <div className="flex flex-col justify-center items-center gap-4">
-      <h2 className="text-3xl text-slate-900 font-semibold dark:text-slate-200">
+      <h2 className="text-3xl text-slate-900 text-center font-semibold dark:text-slate-200">
         {message}
       </h2>
       <Button className="space-x-1" onClick={() => location.reload()}>
